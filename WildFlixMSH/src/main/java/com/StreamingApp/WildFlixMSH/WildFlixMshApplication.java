b682333722f232e19499d68adea2ae5d53d0127e
Mohammed-MW <mohammed.maouedj.pro@gmail.com>
2023-06-30 12:18:27 +0200

    first commit

---
+++ WildFlixMSH/src/main/java/com/StreamingApp/WildFlixMSH/WildFlixMshApplication.java
@@ -0,0 +1,13 @@
+package com.StreamingApp.WildFlixMSH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildFlixMshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildFlixMshApplication.class, args);
+	}
+
+}
---
+++ WildFlixMSH/src/test/java/com/StreamingApp/WildFlixMSH/WildFlixMshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StreamingApp.WildFlixMSH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildFlixMshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
