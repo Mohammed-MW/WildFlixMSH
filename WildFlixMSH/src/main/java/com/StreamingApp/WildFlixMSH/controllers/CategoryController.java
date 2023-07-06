12cfd74252382b6be0c9afab6e952efc31556cb2
Mohammed-MW <mohammed.maouedj.pro@gmail.com>
2023-07-06 09:45:22 +0200

    save avant pull de hana 06/07/2023

---
+++ WildFlixMSH/src/main/java/com/StreamingApp/WildFlixMSH/controllers/CategoryController.java
@@ -0,0 +1,66 @@
+package com.StreamingApp.WildFlixMSH.controllers;
+
+import com.StreamingApp.WildFlixMSH.models.Category;
+import com.StreamingApp.WildFlixMSH.repositories.CategoryRepository;
+import com.StreamingApp.WildFlixMSH.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+
+@Controller
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/admin/categorie")
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/admin/categorie/{id}")
+    public ResponseEntity<Optional<Category>> findById(@RequestBody Long id){
+        Optional<Category> findcatId = categoryService.findById(id);
+        return new ResponseEntity<>(findcatId, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/admin/categorie")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category createdCategory = categoryService.createCategory(category);
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/admin/categorie/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        Category updatedCategory = categoryService.updateCategory(id, category);
+        if (updatedCategory != null) {
+            return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/admin/categorie/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ WildFlixMSH/src/main/java/com/StreamingApp/WildFlixMSH/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.StreamingApp.WildFlixMSH.repositories;
+
+import com.StreamingApp.WildFlixMSH.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
